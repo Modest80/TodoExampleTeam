ba19170f719fe867cbf85f178014e7f1b35183f7
Жанна Ильчук <z-ilchuk@bk.ru>
2024-02-05 21:26:58 +0400

    Add file Todo.cs

--- Todo.cs
+++ Todo.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace TodoExampleTeam {
+    [Serializable]
     public class Todo : ITodo {
         string Title;
         bool isComplete;
@@ -15,20 +16,34 @@ namespace TodoExampleTeam {
         IPerson Giver;
         IPerson Taker;
 
+
+        public Todo() {
+
+        }
+        public Todo(string title, bool isComplete, string description, DateTime dateStart, DateTime dateEnd, IPerson giver, IPerson taker) {
+            Title = title;
+            this.isComplete = isComplete;
+            Description = description;
+            DateStart = dateStart;
+            DateEnd = dateEnd;
+            Giver = giver;
+            Taker = taker;
+        }
+
         public void SetDescription(string description) {
-            
+            Description = description;
         }
 
         public void SetEnd(DateTime end) {
-            
+            DateEnd = end;
         }
 
         public void SetGiver(IPerson giver) {
-            
+            Giver = giver;
         }
 
         public void SetStart(DateTime start) {
-            
+            DateStart = start;
         }
 
         public bool SetStatus(bool isComplete) {
@@ -36,11 +51,14 @@ namespace TodoExampleTeam {
         }
 
         public void SetTaker(IPerson taker) {
-            
+            Taker = taker;
         }
 
         public void SetTitle(string title) {
-            
+            Title=title;
+        }
+        public override string ToString() {
+            return $"{Title}";
         }
     }
 }
